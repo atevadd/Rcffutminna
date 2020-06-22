d30d8b3432dac1f756d439e8a76c67bde7cd4a64
arinzeJeffrey <nnamenearinze@gmail.com>
2020-06-22 18:20:36 +0100

    done with the book routes

--- main_app/routes.py
+++ main_app/routes.py
@@ -3,13 +3,14 @@ import secrets
 from datetime import datetime
 from flask import render_template, url_for, redirect, request,flash
 from main_app import app, db
-from main_app.models import Message, Announcement, Testimony
+from main_app.models import Message, Announcement, Testimony, Book
 
 
 #middleware - save audiofiles
-def save_audio(media, media_title):
+def save_audio(media):
+    random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(media.filename)
-    media_fn = media_title + f_ext
+    media_fn = random_hex + f_ext
     media_path = os.path.join(app.root_path, 'static/message_audios', media_fn)
     media.save(media_path)
     return media_fn
@@ -28,7 +29,7 @@ def save_book(book):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(book.filename)
     book_fn = random_hex + f_ext
-    book_path = os.path.join(app.root_path, 'static/book', book_fn)
+    book_path = os.path.join(app.root_path, 'static/books', book_fn)
     book.save(book_path)
     return book_fn
 
@@ -71,13 +72,50 @@ def edit_announcement(id):
 @app.route('/admin/announcement/<int:id>/delete')
 def delete_announcement(id):
     announce = Announcement.find_by_id(id)
+    os.remove(app.root_path + url_for('static', filename="church_img/"+announce.image))
     announce.remove_from_database()
     flash("Annoucement Deleted Successfully","danger")
     return redirect(url_for('announcement'))
 
-@app.route('/admin/books')
+@app.route('/admin/books', methods=['GET','POST'])
 def books():
-    return render_template('admin/books.html')
+    books = Book.query.all()
+    if request.method == "POST":
+        book = save_book(request.files['book'])
+        new_book = Book(title=request.form['title'], name=book, author=request.form['author'])
+        new_book.save_to_database()
+        flash("Book Added Successfully", "success")
+        return redirect(url_for('books'))
+    return render_template('admin/books.html', books=books)
+
+@app.route('/admin/books/<int:id>/edit', methods=['GET','POST'])
+def edit_book(id):
+    book = Book.find_by_id(id)
+    if request.method == "POST":
+        if request.form['author'] and request.form['title']  and request.files['book']:
+            book_file = save_book(request.files['book'])
+            book.author = request.form['author']
+            book.title = request.form['title']
+            book.name = book_file
+            db.session.commit()
+            flash('Book Updated', "success")
+            return redirect(url_for('books'))
+        elif request.form['author'] and request.form['title']:
+            book.author = request.form['author']
+            book.title = request.form['title']
+            db.session.commit()
+            flash('Book Updated', "success")
+            return redirect(url_for('books'))
+    return render_template('admin/edit_book.html', book=book)
+
+@app.route('/admin/books/<int:id>/delete')
+def delete_book(id):
+    book = Book.find_by_id(id)
+    os.remove(app.root_path + url_for('static', filename="books/"+book.name))
+    print("Book Deleted Successfully!")
+    book.remove_from_database()
+    flash("Book Deleted Successfully","danger")
+    return redirect(url_for('books'))
 
 @app.route('/admin/gallery')
 def gallery():
@@ -88,13 +126,13 @@ def messages():
     messages = Message.query.all()
     if request.method == "POST":
         print(request.form['date'])
-        message_audio = save_audio(request.files['audio'], request.form['title'])
+        message_audio = save_audio(request.files['audio'])
         message = Message(
             minister=request.form['author'], 
             title=request.form['title'], 
             tag=request.form['tag'], 
             message=request.form['message'],
-            audio=message_audio.replace(" ","")
+            audio=message_audio
             )
         message.save_to_database()
         flash("Message Added Successfully", "success")
@@ -104,9 +142,10 @@ def messages():
 @app.route('/admin/messages/<int:id>/edit', methods=['GET','POST'])
 def edit_message(id):
     message = Message.find_by_id(id)
+    print(message.audio)
     if request.method == "POST":
         if request.form['author'] and request.form['title'] and request.form['tag'] and request.files['audio'] and request.form['message']:
-            message_audio = save_audio(request.files['audio'], request.form['title'])
+            message_audio = save_audio(request.files['audio'])
             message.minister = request.form['author']
             message.title = request.form['title']
             message.tag = request.form['tag']
@@ -128,6 +167,7 @@ def edit_message(id):
 @app.route('/admin/messages/<int:id>/delete')
 def delete_message(id):
     message = Message.find_by_id(id)
+    os.remove(app.root_path + url_for('static', filename="message_audios/"+message.audio))
     message.remove_from_database()
     flash("Message Deleted Successfully","danger")
     return redirect(url_for('messages'))
